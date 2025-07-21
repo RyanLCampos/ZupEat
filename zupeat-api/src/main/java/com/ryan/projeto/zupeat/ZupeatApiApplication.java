bdea419277a2829e586349d48fc5c683f7d2b80a
RyanLCampos <ryanlcampos19@gmail.com>
2025-07-21 20:37:01 -0300

    :tada: Commit Inicial

---
+++ zupeat-api/src/main/java/com/ryan/projeto/zupeat/ZupeatApiApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.projeto.zupeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupeatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupeatApiApplication.class, args);
+	}
+
+}
---
+++ zupeat-api/src/test/java/com/ryan/projeto/zupeat/ZupeatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.projeto.zupeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupeatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
