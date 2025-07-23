cfa07823143032ed7d2a7106b731e8778c6f4f03
RyanLCampos <ryanlcampos19@gmail.com>
2025-07-23 17:19:48 -0300

    :construction: Feat: início da implementação das entidades Cidade, Estado, Permissao e FormaPagamento

---
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/model/Cidade.java
@@ -0,0 +1,32 @@
+package io.github.ryanlcampos.zupeat.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity
+@Table
+public class Cidade {
+	
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Estado estado;
+	
+}
