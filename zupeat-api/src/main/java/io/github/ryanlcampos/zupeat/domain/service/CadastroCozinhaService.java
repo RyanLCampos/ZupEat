86974a43e5452d95fbe104347392af4de606435c
RyanLCampos <ryanlcampos19@gmail.com>
2025-07-25 20:25:41 -0300

    :construction: Feat: adição e ajustes na logica do CadastroCozinhaService

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/service/CadastroCozinhaService.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/service/CadastroCozinhaService.java
@@ -1,8 +1,15 @@
 package io.github.ryanlcampos.zupeat.domain.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import io.github.ryanlcampos.zupeat.domain.exceptions.EntidadeEmUsoException;
+import io.github.ryanlcampos.zupeat.domain.exceptions.EntidadeNaoEncontradaException;
 import io.github.ryanlcampos.zupeat.domain.model.Cozinha;
 import io.github.ryanlcampos.zupeat.domain.repository.CozinhaRepository;
 
@@ -14,6 +21,24 @@ public class CadastroCozinhaService {
 	
 	public Cozinha salvar(Cozinha cozinha) {
 		return cozinhaRepository.save(cozinha);
+	}
+	
+	public void remover(Long id) {
+		try {
+			
+			Optional<Cozinha> possivelCozinha = cozinhaRepository.findById(id);
+			
+			if(possivelCozinha.isEmpty()) {
+				throw new EmptyResultDataAccessException(1);
+			}
+			
+			cozinhaRepository.deleteById(id);
+			
+		}catch(DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format("Cozinha de codigo %d não pode ser removida, pois está em uso", id));
+		}catch(EmptyResultDataAccessException er) {
+			throw new EntidadeNaoEncontradaException(String.format("Cozinha de codigo %d não foi encontrada", id));
+		}
 	}
 	
 }
