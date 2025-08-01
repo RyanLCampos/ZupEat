b4bb4bbfdcfc33f141d094e624608788e8e91496
RyanLCampos <ryanlcampos19@gmail.com>
2025-08-01 11:15:32 -0300

    :recycle: Refactor: realiza ajustes em CozinhaRepository

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/repository/CozinhaRepository.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/repository/CozinhaRepository.java
@@ -1,13 +1,9 @@
 package io.github.ryanlcampos.zupeat.domain.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.ryanlcampos.zupeat.domain.model.Cozinha;
 
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
-	
-	Cozinha findByNome(String nome);
-	
+
 }
