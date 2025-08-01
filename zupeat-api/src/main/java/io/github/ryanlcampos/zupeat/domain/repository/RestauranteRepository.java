3ab0491cda835dceb793179981f8fa14ff6f8426
RyanLCampos <ryanlcampos19@gmail.com>
2025-08-01 12:16:55 -0300

    :sparkles: Feat: adiciona query JPQL customizada em RestauranteRepository e externaliza consultas para XML

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/repository/RestauranteRepository.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/repository/RestauranteRepository.java
@@ -3,7 +3,12 @@ package io.github.ryanlcampos.zupeat.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.ryanlcampos.zupeat.domain.model.Restaurante;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
-	
+
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
 }
