235677aa3000778a788190e77dcc770ff09a133e
RyanLCampos <ryanlcampos19@gmail.com>
2025-08-06 20:52:54 -0300

    :recycle: Refactor: realiza ajustes em EntidadeEmUsoException e EntidadeNaoEncontradaException

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/EntidadeEmUsoException.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/EntidadeEmUsoException.java
@@ -1,11 +1,16 @@
 package io.github.ryanlcampos.zupeat.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class EntidadeEmUsoException extends RuntimeException{
 
-	/**
-	 */
+	@Serial
 	private static final long serialVersionUID = 1L;
-	
+
 	public EntidadeEmUsoException(String mensagem) {
 		super(mensagem);
 	}
--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/EntidadeNaoEncontradaException.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/EntidadeNaoEncontradaException.java
@@ -1,13 +1,19 @@
 package io.github.ryanlcampos.zupeat.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException{
 
-	/**
-	 */
+
+	@Serial
 	private static final long serialVersionUID = 1L;
-	
+
 	public EntidadeNaoEncontradaException(String mensagem) {
 		super(mensagem);
 	}
-	
+
 }
