b23ab656ba78b33221a59f1d022c28bd0571a32e
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-16 18:58:57 -0300

    :sparkles: Feat: adiciona JsonIgnoreProperties em atributo Cozinha

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/model/Restaurante.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/model/Restaurante.java
@@ -5,19 +5,34 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import io.github.ryanlcampos.zupeat.core.validation.Groups;
 import io.github.ryanlcampos.zupeat.core.validation.ValorZeroIncluiDescricao;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.groups.ConvertGroup;
 import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @ValorZeroIncluiDescricao(valorField = "taxaFrete",
 			descricaoField = "nome", descricaoObrigatoria = "Frete Grátis")
@@ -41,6 +56,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
+	@JsonIgnoreProperties(value = "nome", allowGetters = true)
 	@Valid
 	@ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
 	@NotNull
