6960e4f2bf12c2e59052136b071fdb3525796607
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-17 03:45:26 -0300

    :recycle: Refactor: refatorando para utilizar model mapper

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/assembler/RestauranteInputDisassembler.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/assembler/RestauranteInputDisassembler.java
@@ -1,25 +1,19 @@
 package io.github.ryanlcampos.zupeat.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import io.github.ryanlcampos.zupeat.api.model.input.RestauranteInput;
-import io.github.ryanlcampos.zupeat.domain.model.Cozinha;
 import io.github.ryanlcampos.zupeat.domain.model.Restaurante;
 
 @Component
 public class RestauranteInputDisassembler {
+
+	@Autowired
+	private ModelMapper modelMapper;
     
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-		Restaurante restaurante = new Restaurante();
-
-		restaurante.setNome(restauranteInput.getNome());
-		restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-		Cozinha cozinha = new Cozinha();
-		cozinha.setId(restauranteInput.getCozinha().getId());
-
-		restaurante.setCozinha(cozinha);
-
-		return restaurante;
+		return modelMapper.map(restauranteInput, Restaurante.class);
 	}
 }
--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/assembler/RestauranteModelAssembler.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/assembler/RestauranteModelAssembler.java
@@ -3,30 +3,21 @@ package io.github.ryanlcampos.zupeat.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import io.github.ryanlcampos.zupeat.api.model.CozinhaModel;
 import io.github.ryanlcampos.zupeat.api.model.RestauranteModel;
 import io.github.ryanlcampos.zupeat.domain.model.Restaurante;
 
 @Component
 public class RestauranteModelAssembler {
     
+	@Autowired
+	private ModelMapper modelMapper;
 
     public RestauranteModel toModel(Restaurante restaurante) {
-		CozinhaModel cozinhaModel = new CozinhaModel();
-
-		cozinhaModel.setId(restaurante.getCozinha().getId());
-		cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-		RestauranteModel restauranteModel = new RestauranteModel();
-
-		restauranteModel.setId(restaurante.getId());
-		restauranteModel.setNome(restaurante.getNome());
-		restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-		restauranteModel.setCozinha(cozinhaModel);
-
-		return restauranteModel;
+		return modelMapper.map(restaurante, RestauranteModel.class);
 	}
 
 	public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
