485f86be1456107e892ce2d8d7d4d0d8565a5157
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-18 19:33:33 -0300

    :sparkles: Feat: implementa DTO para Endereco e adiciona endereco no DTO de restaurante

---
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/model/CidadeResumoModel.java
@@ -0,0 +1,14 @@
+package io.github.ryanlcampos.zupeat.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {    
+    
+    private Long id;
+    private String nome;
+    private String estado;
+
+}
---
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/model/EnderecoModel.java
@@ -0,0 +1,17 @@
+package io.github.ryanlcampos.zupeat.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EnderecoModel {
+
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private CidadeResumoModel cidade;
+
+}
