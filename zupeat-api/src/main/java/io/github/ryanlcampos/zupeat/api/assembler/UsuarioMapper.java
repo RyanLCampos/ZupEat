e0e2b6a84f38aea0db13c51ffc770ba1aa032faa
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-23 12:12:49 -0300

    :sparkles: feat(usuario): implementa UsuarioMapper para conversão entre domain e DTO

---
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/api/assembler/UsuarioMapper.java
@@ -0,0 +1,42 @@
+package io.github.ryanlcampos.zupeat.api.assembler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.github.ryanlcampos.zupeat.api.model.UsuarioModel;
+import io.github.ryanlcampos.zupeat.api.model.input.UsuarioInput;
+import io.github.ryanlcampos.zupeat.domain.model.Usuario;
+
+@Component
+public class UsuarioMapper {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    // DOMAIN -> DTO
+
+    public UsuarioModel toModel(Usuario usuario) {
+        return modelMapper.map(usuario, UsuarioModel.class);
+    }
+
+    public List<UsuarioModel> toCollectionModel(List<Usuario> usuarios) {
+        return usuarios.stream()
+                .map(usuario -> toModel(usuario))
+                .collect(Collectors.toList());
+    }
+
+    // DTO -> DOMAIN
+
+    public Usuario toDomainObject(UsuarioInput usuarioInput) {
+        return modelMapper.map(usuarioInput, Usuario.class);
+    }
+
+    public void copyToDomainObject(UsuarioInput usuarioInput, Usuario usuario) {
+        modelMapper.map(usuarioInput, usuario);
+    }
+
+}
