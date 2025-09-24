866349db10e1164d0350e02fb970930969347de7
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-24 12:22:07 -0300

    :hammer_and_wrench: feat(repository): adiciona @EnableJpaRepositories com CustomJpaRepositoryImpl

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/ZupeatApiApplication.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/ZupeatApiApplication.java
@@ -4,8 +4,12 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.github.ryanlcampos.zupeat.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class ZupeatApiApplication {
 
 	public static void main(String[] args) {
