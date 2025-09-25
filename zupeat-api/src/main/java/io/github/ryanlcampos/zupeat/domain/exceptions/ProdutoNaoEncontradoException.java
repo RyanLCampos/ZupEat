2db5f9ac24bd09469a3523853f5e6d056083f27a
Ryan Lucas <ryanlcampos19@gmail.com>
2025-09-25 13:57:34 -0300

    :recycle: refactor(produto): altera mensagem de erro

--- zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/ProdutoNaoEncontradoException.java
+++ zupeat-api/src/main/java/io/github/ryanlcampos/zupeat/domain/exceptions/ProdutoNaoEncontradoException.java
@@ -2,17 +2,18 @@ package io.github.ryanlcampos.zupeat.domain.exceptions;
 
 import java.io.Serial;
 
-public class ProdutoNaoEncontradoException extends EntidadeNaoEncontradaException{
+public class ProdutoNaoEncontradoException extends EntidadeNaoEncontradaException {
 
     @Serial
-	private static final long serialVersionUID = 1L;
-    
+    private static final long serialVersionUID = 1L;
+
     public ProdutoNaoEncontradoException(String mensagem) {
         super(mensagem);
     }
 
-    public ProdutoNaoEncontradoException(Long produtoId){
-        this(String.format("Produto de codigo %d não foi encontrado", produtoId));
+    public ProdutoNaoEncontradoException(Long restauranteId, Long produtoId) {
+        this(String.format("Produto de codigo %d não foi encontrado para o restaurante de código %d",
+                produtoId, restauranteId));
     }
-    
+
 }
